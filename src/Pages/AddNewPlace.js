37b1a2c270f5cdd911a1052979a06ad32181d8b6
Shumieb <shumiebc@gmail.com>
2024-01-23 22:05:15 +0000

    Added react router and added Pages

---
+++ src/Pages/AddNewPlace.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddNewPlace() {
+    return (
+        <div>AddNewPlace</div>
+    )
+}
+
+export default AddNewPlace
--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
-import App from './App';
+import LayoutPage from './Pages/LayoutPage';
+import HomePage from "./Pages/HomePage"
+import PlacesPage from './Pages/PlacesPage';
+import PlacePage from './Pages/PlacePage';
+import AddNewPlace from './Pages/AddNewPlace';
+import EditPlacePage from './Pages/EditPlacePage';
+import AboutPage from "./Pages/AboutPage";
+import NoPage from "./Pages/NoPage";
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutPage />}>
+          <Route index element={<HomePage />} />
+          <Route path="places" element={<PlacesPage />} >
+            <Route path=":placeId" element={<PlacePage />} />
+            <Route path="new" element={<AddNewPlace />} />
+            <Route path="edit" element={<EditPlacePage />} />
+          </Route>
+          <Route path="about" element={<AboutPage />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 
