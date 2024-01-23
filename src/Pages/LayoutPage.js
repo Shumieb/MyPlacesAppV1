75490a098f72e935038adb3b50600b9dce322e25
Shumieb <shumiebc@gmail.com>
2024-01-23 22:09:16 +0000

    Added Pages and components

--- src/Pages/LayoutPage.js
+++ src/Pages/LayoutPage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import NavBar from '../Components/NavBar'
+import Footer from '../Components/Footer'
 
 function LayoutPage() {
     return (
-        <div>LayoutPage</div>
+        <div>
+            <NavBar />
+            <Footer />
+        </div>
     )
 }
 
